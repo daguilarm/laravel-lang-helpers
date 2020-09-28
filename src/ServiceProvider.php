eb7b098caa415f30b5b6ddb8d149f3364ac03e1c
daguilarm <damian.aguilarm@gmail.com>
2020-09-28 08:37:34 +0200

    updated

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Daguilarm\Lang;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider as Provider;
 
 final class ServiceProvider extends Provider
